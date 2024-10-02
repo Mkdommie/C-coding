0e812fc6dfbd233fe7ed63ff77e80ced69346d4b
Mkdommie <mkdommie19@gmail.com>
2024-10-02 19:01:00 +0300

    Add files via upload

---
+++ variables data types .c
@@ -0,0 +1,21 @@
+//variables and data types 
+//prepossesor directives- scanf(), printf()
+
+#include< stdio.h >
+int main () {
+     // declaration and initialization 
+     char a='s';//
+     char name []=("Dominic");
+     int age =19;
+     float salary =20000;
+    double budget =10000;
+
+printf ("the character is%c\n ",a);
+printf ("the name is %s\n",name);
+printf ("the age is %d\n",age);
+printf ("the salary is%f\n",salary);
+printf("the budget is%Lf\n",budget);
+
+return 0;
+}
+     
